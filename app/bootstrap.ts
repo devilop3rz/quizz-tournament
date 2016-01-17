59da8a609db4b093f1a8e9e0550f6f62ed471a8f
Marvin Ernst <devilop3rz@gmail.com>
2016-01-17 09:30:12 +0100

    Added Bootstrap

--- app/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {provide}    from 'angular2/core'
-import {CORE_DIRECTIVES} from 'angular2/common'
-import {ROUTER_BINDINGS, ROUTER_PROVIDERS, LocationStrategy, PathLocationStrategy} from 'angular2/router'
