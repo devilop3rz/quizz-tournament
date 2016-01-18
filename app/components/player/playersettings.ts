431533e9742d61639ffddf96120c3dcaa245ceeb
Marvin Ernst <ernst@cognitio.de>
2016-01-18 11:09:09 +0100

    Added forms to create playersettings

--- app/components/player/playersettings.ts
+++ app/components/player/playersettings.ts
@@ -1,11 +1,13 @@
 import {Component} from 'angular2/core'
 import {NgForm} from 'angular2/common'
+import {Router} from 'angular2/router'
 
 class Player {
 
 	constructor(
-		public name: string,
-		public pic: string
+		public name:string,
+		public pic:string,
+		public color:string
 	) {}
 	
 }
@@ -13,21 +15,45 @@ class Player {
 @Component({
 	selector: 'gamepad',
 	template: `
-		
-		<form (ngSubmit)="onSubmit()">
-			<input type="text" [(ngModel)]="player.name"/>
-		</form>
+		{{diagnostic}}
 
+		<h1>Setup your Player</h1>
+
+		<form (ngSubmit)="onSubmit()">
+			<div class="form-group">
+				<label for="player-name">Name:</label>
+				<input type="text" id="player-name" [(ngModel)]="player.name"/>
+			</div>
+			<div class="form-group">
+				<label for="player-pic">Char:</label>
+				<select name="player-pic" id="player-pic" [(ngModel)]="player.pic">
+					<option>SpielerChar1</option>
+					<option>SpielerChar2</option>
+					<option>SpielerChar3</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="player-color">Color:</label>
+				<input type="color" id="player-color" [(ngModel)]="player.color"/>
+			</div>
+			<button type="submit">Ready</button>
+		</form>
 	`
 })
 
 export class PlayerSettings {
 
-	public player:Player = new Player('Default', '');
-
-	onsubmit() {
+	public player:Player = new Player('Default', '', '#123456');
+	constructor(
+		public _router:Router
+	)
+	{}
+    onSubmit() {
 		// Set the player settings and send them over to the server
 		console.log('Set player setting:' + this.player.name);
+		this._router.navigate(['/Lobby'])
 	}
+
+    get diagnostic() { return JSON.stringify(this.player); }
 
 }
--- public/js/components/player/playersettings.js
+++ public/js/components/player/playersettings.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,35 +8,46 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1;
     var Player, PlayerSettings;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             Player = (function () {
-                function Player(name, pic) {
+                function Player(name, pic, color) {
                     this.name = name;
                     this.pic = pic;
+                    this.color = color;
                 }
                 return Player;
             })();
             PlayerSettings = (function () {
-                function PlayerSettings() {
-                    this.player = new Player('Default', '');
+                function PlayerSettings(_router) {
+                    this._router = _router;
+                    this.player = new Player('Default', '', '#123456');
                 }
-                PlayerSettings.prototype.onsubmit = function () {
+                PlayerSettings.prototype.onSubmit = function () {
                     // Set the player settings and send them over to the server
                     console.log('Set player setting:' + this.player.name);
+                    this._router.navigate(['/Lobby']);
                 };
+                Object.defineProperty(PlayerSettings.prototype, "diagnostic", {
+                    get: function () { return JSON.stringify(this.player); },
+                    enumerable: true,
+                    configurable: true
+                });
                 PlayerSettings = __decorate([
                     core_1.Component({
                         selector: 'gamepad',
-                        template: "\n\t\t\n\t\t<form (ngSubmit)=\"onSubmit()\">\n\t\t\t<input type=\"text\" [(ngModel)]=\"player.name\"/>\n\t\t</form>\n\n\t"
+                        template: "\n\t\t{{diagnostic}}\n\n\t\t<h1>Setup your Player</h1>\n\n\t\t<form (ngSubmit)=\"onSubmit()\">\n\t\t\t<div class=\"form-group\">\n\t\t\t\t<label for=\"player-name\">Name:</label>\n\t\t\t\t<input type=\"text\" id=\"player-name\" [(ngModel)]=\"player.name\"/>\n\t\t\t</div>\n\t\t\t<div class=\"form-group\">\n\t\t\t\t<label for=\"player-pic\">Char:</label>\n\t\t\t\t<select name=\"player-pic\" id=\"player-pic\" [(ngModel)]=\"player.pic\">\n\t\t\t\t\t<option>SpielerChar1</option>\n\t\t\t\t\t<option>SpielerChar2</option>\n\t\t\t\t\t<option>SpielerChar3</option>\n\t\t\t\t</select>\n\t\t\t</div>\n\t\t\t<div class=\"form-group\">\n\t\t\t\t<label for=\"player-color\">Color:</label>\n\t\t\t\t<input type=\"color\" id=\"player-color\" [(ngModel)]=\"player.color\"/>\n\t\t\t</div>\n\t\t\t<button type=\"submit\">Ready</button>\n\t\t</form>\n\t"
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [router_1.Router])
                 ], PlayerSettings);
                 return PlayerSettings;
             })();
